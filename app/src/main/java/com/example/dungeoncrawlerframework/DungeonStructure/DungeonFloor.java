769a8a9bf55ac0af4be9b6b2f391e30b86a84207
Edward <edward.w.lin@gmail.com>
2020-11-03 23:01:40 -0500

    Flesh out Dungeon Floor and Dungeon Floor Dict functional road map (20201103)

--- app/src/main/java/com/example/dungeoncrawlerframework/DungeonStructure/DungeonFloor.java
+++ app/src/main/java/com/example/dungeoncrawlerframework/DungeonStructure/DungeonFloor.java
@@ -9,7 +9,8 @@ public class DungeonFloor {
 
     //todo: [Medium] Clean up code formatting (i.e. white space and comments)
 
-    private MonsterDictionary monsterDictionary = new MonsterDictionary();
+    private final MonsterDictionary monsterDictionary = new MonsterDictionary();
+    private DungeonFloorDictionary dungeonFloorDictionary;
     private ArrayList<Monster> floorMonsters;
     private int floorNumber;
 
@@ -19,15 +20,19 @@ public class DungeonFloor {
         //todo:[CRITICAL] randomly generate a list of monsters that you would fight based on the population of the Dungeon Floor with one boss monster at the end
     }
 
-    public MonsterDictionary getMonsterDictionary() {
-        return monsterDictionary;
-    }
+    //todo:[Step 1] take dungeon floorNumber and pass it to a DungeonFloorDict obj to return a list of possible monsters available on this floor
+    //todo:[Step 2] take the list of possible monsters on this floor their associated probabilities of showing up and generate a list of monsters for this floor (return a hash map after invoking .getFloor(int floorNumber)
+    //todo:[Step 3] reference the list in the Battle Activity where each Monster that shows up is simply another record in that list
+    //todo:[Note]each floor on the floor dict should take a hast map of monsters as well as a single monster as the floor boss for that dungeon
+    //todo:[Step 4]create variables for the dungeon floor and stage of progress in battle activity
+    //todo:[Step 5] create variables for the dungeon floor and stage within the player for progress to be saved and associated with the player
+    //todo:[Step 6] create a "Map" activity so that players can select the dungeon floor they want to tackle at any point in time.
 
-    public void setMonsterDictionary(MonsterDictionary monsterDictionary) {
-        this.monsterDictionary = monsterDictionary;
-    }
+
+
 
     public ArrayList<Monster> getFloorMonsters() {
+
         return floorMonsters;
     }
 
